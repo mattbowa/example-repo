c827233c9dd6d06bdae60b6fb448102ab14e5def
matt.bowa <matt.bowa@mail.com>
2020-08-20 06:17:25 +0000

    My first commitgit status

---
+++ example.js
@@ -0,0 +1,31 @@
+matt.bowa@mail.com
+
+
+git config --global user.name matt.bowa
+git config --global user.email matt.bowa@mail.com
+
+$ ssh-keygen -t rsa -C matt.bowa@mail.com
+
+$ ssh-keygen -t rsa -b 4096 -C matt.bowa@mail.com
+
+
+
+DOMAIN=http://192.168.0.74:3000
+AUTH0_CLIENT_ID=Kg6nPtTz8kAciAuvMpC9fgQlETy2Bi24
+AUTH0_AUDIENCE=https://hasura.io/learn
+AUTH0_DOMAIN=dev-hgr-43bm.au.auth0.com
+AUTH0_CLIENT_SECRET=0LMBu1ZtVNHVnInHpMdgBXLu4m42afo5XG0aB4ytgQnsyOnWhbI3Nrl-smVvQ9wq
+REDIRECT_URI=http://192.168.0.74:3000/api/callback
+POST_LOGOUT_REDIRECT_URI=http://192.168.0.74:3000/
+SESSION_COOKIE_SECRET=Downloadtheboilerplatefromhasura.io/learn/graphql/nextjs-fullstack-serverless/boilerplate.zip
+
+
+ http://192.168.0.74:3000/callback
+ 
+ http://192.168.0.74:3000/callback
+ 
+ http://192.168.0.74:3000/callback
+ 
+ 
+
+
